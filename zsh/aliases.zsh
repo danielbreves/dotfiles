88504a3e4188d6bf8a1a182b03104df9652c2923
Daniel Ribeiro <djbreves@gmail.com>
2015-03-27 16:02:19 +1100

    Updates Sublime and add noglob rake as an alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,6 +43,7 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 alias migrate='rake db:migrate db:test:clone'
+alias rake='noglob rake'
 
 # GITHUB
 # ********************************************************
