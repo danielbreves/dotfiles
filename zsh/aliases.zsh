ab922cc22bd369c0403391d0300d57946164c7e5
Daniel Ribeiro <djbreves@gmail.com>
2019-01-20 15:31:42 +1100

    Remove Zendesk and add docker-compose alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -70,38 +70,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 
-# ZENDESK
-# ********************************************************
-
-# Paths
-if [ -d "${HOME}/Code/zendesk" ]; then
-  export ZENDESK_ROOT=~/Code/zendesk
-  export SSH_USER=dribeiro
-  export ZENDESK_APP_MARKET_PATH=~/Code/zendesk/zendesk_app_market
-  export ZENDESK_APPS_TOOLS_PATH=~/Code/zendesk/zendesk_apps_tools
-  export ZENDESK_CARSON_PATH=~/Code/zendesk/zendesk_carson
-  export ZENDESK_CLASSIC_PATH=~/Code/zendesk/zendesk
-  export ZENDESK_PROVISIONING_PATH=~/Code/zendesk/zendesk_provisioning
-
-  # Server configuration
-  #export ZENDESK_HOST=zendesk.dev
-  #export ZENDESK_PORT=443
-
-  # Aliases
-  alias apps="cd $ZENDESK_ROOT/apps"
-  alias classic="cd $ZENDESK_CLASSIC_PATH"
-  alias carson="cd $ZENDESK_CARSON_PATH"
-  alias console="cd $ZENDESK_ROOT/zendesk_console"
-  alias zaf="cd $ZENDESK_ROOT/zendesk_app_framework"
-  alias zas="cd $ZENDESK_ROOT/zendesk_apps_support"
-  alias lotus="cd $ZENDESK_ROOT/zendesk_console"
-  alias zam="cd $ZENDESK_APP_MARKET_PATH"
-  alias zd="cd $ZENDESK_ROOT"
-  alias tfcl="tail -f $ZENDESK_CARSON_PATH/log/development.log"
-  alias reload-all="boxen --restart-service nginx"
-  alias reload-carson="touch $ZENDESK_CARSON_PATH/tmp/restart.txt"
-  alias console_carson="pushd $ZENDESK_CARSON_PATH > /dev/null; bundle exec rails console; popd > /dev/null"
-  alias console_classic="pushd $ZENDESK_CLASSIC_PATH > /dev/null; bundle exec rails console; popd > /dev/null"
-  alias console_jig="pushd $ZENDESK_ROOT/jig > /dev/null; bundle exec rails console; popd > /dev/null"
-  alias console_hercules="pushd $ZENDESK_ROOT/hercules > /dev/null; bundle exec rails console; popd > /dev/null"
-fi
+# DOCKER
+alias dcea='docker-compose exec app'
